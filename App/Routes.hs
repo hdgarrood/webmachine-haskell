edddcfd3d691bc8d2abbfb315ba5a1a1ffa03ae7
Harry Garrood <hdgarrood@gmail.com>
2012-10-11 02:26:12 +0100

    add create and show actions

--- App/Routes.hs
+++ App/Routes.hs
@@ -2,38 +2,45 @@
 
 module App.Routes where
 
-import Data.Text (Text)
+import Data.Text (Text, append)
 import Control.Monad
 import Happstack.Server
 
 routes :: ServerPart Response
 routes = resources
 
+-- like the resources method in Rails; should provide 7 RESTful actions
+-- for a resource.
 resources :: ServerPart Response
 resources = msum
-    [ nullDir >> index
-    , dir "new" $ new
+    [ nullDir >> msum
+        [ method GET >> indexR
+        , method POST >> createR
+        ]
+    , dir "new" $ newR
+    , path showR
     ]
 
 simpleResponse :: Text -> ServerPart Response
 simpleResponse text = ok $ toResponse text
 
-new :: ServerPart Response
-new = simpleResponse "Form to make a new thingy with\n"
+indexR :: ServerPart Response
+indexR = simpleResponse "A list of thingies\n"
 
-index :: ServerPart Response
-index = simpleResponse "A list of thingies\n"
+newR :: ServerPart Response
+newR = simpleResponse "Form to make a new thingy with\n"
 
-aroutes :: ServerPart Response
-aroutes = method [GET, HEAD] >> subRoutes
+createR :: ServerPart Response
+createR = simpleResponse "Created a thingy\n"
 
-subRoutes :: ServerPart Response
-subRoutes = msum 
-       [ do methodM GET
-            ok $ toResponse ("You did a GET request.\n" :: Text)
-         , do methodM POST
-              ok $ toResponse ("You did a POST request.\n" :: Text)
-         , dir "foo" $ do methodM GET
-                          ok $ toResponse ("You did a GET request on /foo\n" :: Text)
-       ]
+showR :: Text -> ServerPart Response
+showR id_ = simpleResponse (append (append "Here's a thingy: " id_) "\n")
 
+editR :: ServerPart Response
+editR = simpleResponse "Form to edit a thingy\n"
+
+updateR :: ServerPart Response
+updateR = simpleResponse "Updated a thingy\n"
+
+destroyR :: ServerPart Response
+destroyR = simpleResponse "Deleted a thingy\n"
