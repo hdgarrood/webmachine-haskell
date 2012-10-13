9d93b1e8c65aea408e73c791e3a29dc52739b3b5
Harry Garrood <hdgarrood@gmail.com>
2012-10-13 23:07:04 +0100

    converting to WAI. nearly works.

--- webmachine-port/main.hs
+++ webmachine-port/main.hs
@@ -1,70 +1,91 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad (msum)
+import Control.Monad.Reader
 import Safe (readMay)
-import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Lazy.Char8 as BS8 (pack) 
 
-import qualified Happstack.Server as HS
-import Happstack.Server.Types (Method(..))
+import qualified Network.Wai as W
+import qualified Network.HTTP.Types as H
+import Network.Wai.Handler.Warp (run)
+
+-- combines ability to read the request with doing IO actions
+type ServerMonad = ReaderT W.Request IO
 
 class Resource a where
-    serviceAvailable :: a -> HS.ServerPart Bool
-    serviceAvailable = const (return True) 
+    serviceAvailable :: a -> ServerMonad Bool
+    serviceAvailable = const $ return True
 
-    knownMethods :: a -> HS.ServerPart [HS.Method]
-    knownMethods _ = return [GET, HEAD, POST, PUT, DELETE, OPTIONS, TRACE]
+    knownMethods :: a -> ServerMonad [H.Method]
+    knownMethods = const $ return 
+        [ "GET"
+        , "HEAD"
+        , "POST"
+        , "PUT"
+        , "DELETE"
+        , "OPTIONS"
+        , "TRACE"
+        ]
 
-    allowedMethods :: a -> HS.ServerPart [HS.Method]
-    allowedMethods a = knownMethods a
+    allowedMethods :: a -> ServerMonad [H.Method]
+    allowedMethods = knownMethods
 
-    toHtml :: a -> HS.ServerPart T.Text
-    toText :: a -> HS.ServerPart T.Text
+    toHtml :: a -> ServerMonad BS.ByteString
+    toText :: a -> ServerMonad BS.ByteString
 
 data PostsCollection = PostsCollection
 
 newtype PostId = PostId Int deriving (Show, Read, Eq, Ord)
 data Post = Post PostId
 
-instance HS.FromReqURI PostId where
-    fromReqURI = readMay
-
 instance Resource PostsCollection where
-    toText _ = return "PostsCollection: Here are all the posts."
+    toText _ = return "PostsCollection: Here are all the posts.\n"
 
 instance Resource Post where
-    toText (Post postId) = return $ "This is the page for post" `T.append` (T.pack . show $ postId)
+    toText (Post postId) = return $
+        "This is the page for post"
+        `BS.append` (BS8.pack $ show $ postId)
+        `BS.append` ".\n"
 
 -- the main entry point. Takes a resource and returns a response
-handle :: (Resource a) => a -> HS.ServerPart HS.Response
+handle :: (Resource a) => a -> ServerMonad W.Response
 handle res = do
     available <- serviceAvailable res
     if available
         then do
             knownMeths <- knownMethods res
-            HS.Request {HS.rqMethod = rqMeth} <- HS.askRq
-            if HS.matchMethod knownMeths rqMeth
+            W.Request {W.requestMethod = rqMeth} <- ask
+            if rqMeth `elem` knownMeths
                 then do
                     allowedMeths <- allowedMethods res
-                    if HS.matchMethod allowedMeths rqMeth
+                    if rqMeth `elem` allowedMeths
                         then do
                             text <- toText res
-                            return $ HS.toResponse text
+                            return $ W.responseLBS H.ok200 [] text
                         -- TODO: list of allowed methods in Accept header
                         else methodNotAllowed 
                 else notImplemented
         else serviceUnavailable
 
-methodNotAllowed :: HS.ServerPart HS.Response
-methodNotAllowed = HS.resp 405 (HS.toResponse T.empty)
+methodNotAllowed :: ServerMonad W.Response
+methodNotAllowed = return $ W.responseLBS H.status405 [] ""
 
-notImplemented :: HS.ServerPart HS.Response
-notImplemented = HS.resp 501 (HS.toResponse T.empty)
+notImplemented :: ServerMonad W.Response
+notImplemented = return $ W.responseLBS H.status501 [] ""
 
-serviceUnavailable :: HS.ServerPart HS.Response
-serviceUnavailable = HS.resp 503 (HS.toResponse T.empty)
+serviceUnavailable :: ServerMonad W.Response
+serviceUnavailable = return $ W.responseLBS H.status503 [] ""
+
+app :: W.Application
+app req = case W.rawPathInfo req of
+            "/posts"    -> runReaderT (handle PostsCollection) req
+            "/posts/1"  -> runReaderT (handle Post (PostId 1)) req
+            _           -> return $ H.status404 [] "LULZ"
 
 main :: IO ()
-main = HS.simpleHTTP HS.nullConf $ msum
-    [ HS.dir "posts" $ HS.nullDir >> handle PostsCollection
-    , HS.dir "posts" $ HS.path (\postId -> handle (Post postId))
-    ]
+main = run 3000 app
+--main :: IO ()
+--main = HS.simpleHTTP HS.nullConf $ msum
+--    [ HS.dir "posts" $ HS.nullDir >> handle PostsCollection
+--    , HS.dir "posts" $ HS.path (\postId -> handle (Post postId))
+--    ]
